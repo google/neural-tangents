Introducing a richer set of options for covariances stored in `var1`/`var2` and  `nngp`/`ntk`. This fixes the bug in average pooling which requires pixel-pixel covariances to be tracked in `var1`/`var2` and prepares the ground for incorporation of attention layers.
1) Adding `marginal` and `cross` attributes to `utils.Kernel`,
specifying covariances between which dimensions are to be tracked in
`var1`/`var2` and `nngp`/`ntk` respectively.

2) Methods `_input_to_kernel`, `_transform_kernels_ab_relu`,
`_transform_kernels_erf`, and the layers `Conv`, `Flatten`, `AvgPool`,
and `GlobalAvgPool` are now capable of computing `var1`\`var2`
pixel-pixel covariances.

3) Fixes the bug in `AvgPoll` and `GlobalAvgPool`. Both now correctly
use pixel-pixel covariances in the computation of `ker_fun`.

Tests: all pass modulo the pre-existing issues with `predict_test_tpu`
and one monte_carlo_test which times out

http://sponge/9a2e0d5d-2292-4096-aeab-1e579e2ce348

PiperOrigin-RevId: 270403443

neural_tangents/utils/batch.py
@@ -81,7 +81,9 @@ def _flatten_kernel(k):
         _flatten_batch_dimensions(k.var2, discard_axis=0),
         _flatten_batch_dimensions(k.ntk),
         np.all(k.is_gaussian) if k.is_gaussian is not None else None,
-        np.all(k.is_height_width) if k.is_height_width  is not None else None)
+        np.all(k.is_height_width) if k.is_height_width is not None else None,
+        np.reshape(k.marginal, (-1,))[0] if k.marginal is not None else None,
+        np.reshape(k.cross, (-1,))[0] if k.cross is not None else None)
   elif isinstance(k, np.ndarray):
     return _flatten_batch_dimensions(k)
   else:
@@ -100,7 +102,9 @@ def _move_kernel_to_cpu(kernel):
         device_get(kernel.var2),
         device_get(kernel.ntk),
         kernel.is_gaussian,
-        kernel.is_height_width)
+        kernel.is_height_width,
+        kernel.marginal,
+        kernel.cross)
   else:
     return device_get(kernel)
 