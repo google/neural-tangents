a0baf17ab3e40079cbc2fff487be48f5f4be989e
Roman Novak <romann@google.com>
2019-11-11 10:00:09 -0800

    Update setuptools to include various metadata to show on https://pypi.org/project/neural-tangents/. Also update minimum jax version.
    PiperOrigin-RevId: 279758960

--- setup.py
+++ setup.py
@@ -12,21 +12,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
+"""Setup the package with pip."""
+
+
+import os
 import setuptools
+
+# https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
 
 
 INSTALL_REQUIRES = [
-    'jaxlib',
-    'jax',
+    'jaxlib>=0.1.32',
+    'jax>=0.1.50',
 ]
+
 
 setuptools.setup(
     name='neural-tangents',
-    version='0.1.0',
+    version='0.1.1',
     license='Apache 2.0',
     author='Google',
     author_email='neural-tangents-dev@google.com',
     install_requires=INSTALL_REQUIRES,
     url='https://github.com/google/neural-tangents',
-    packages=setuptools.find_packages()
-    )
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    description='Fast and Easy Infinite Neural Networks in Python',
+    python_requires='>=2.7',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux'
+    ]
+)
