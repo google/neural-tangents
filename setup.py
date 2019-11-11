140d210a2f8f5d1c26c42ef1cd07a43d4ba0b831
Roman Novak <romann@google.com>
2019-11-11 11:19:20 -0800

    Fix setuptools for Python 2.7
    PiperOrigin-RevId: 279777543

--- setup.py
+++ setup.py
@@ -16,12 +16,13 @@
 """Setup the package with pip."""
 
 
+import io
 import os
 import setuptools
 
 # https://packaging.python.org/guides/making-a-pypi-friendly-readme/
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with io.open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
 
