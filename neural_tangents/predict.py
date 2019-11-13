Fix NTK covariance formula to be symmetric. Previous code computed variance correctly but did not return correct symmetric covariance.
Change `compute_var` keyword argument to `compute_cov`.

Fix correct reference for ODE solver.

PiperOrigin-RevId: 280218312

neural_tangents/predict.py
@@ -107,7 +107,7 @@ def gradient_descent_mse(g_dd, y_train, g_td=None, diag_reg=0.):
   normalization = y_train.size
   output_dimension = y_train.shape[-1]
   expm1_fn, inv_expm1_fn = (_make_expm1_fn(normalization),
-                                _make_inv_expm1_fn(normalization))
+                            _make_inv_expm1_fn(normalization))
 
   def fl(fx):
     """Flatten outputs."""
@@ -165,7 +165,7 @@ def gradient_descent(g_dd, y_train, loss, g_td=None):
 
   This function uses the scipy ode solver with the 'dopri5' algorithm.
 
-  [*] https://arxiv.org/abs/1806.07572
+  [*] https://arxiv.org/abs/1902.06720
 
   Example:
     ```python
@@ -299,7 +299,7 @@ def momentum(g_dd, y_train, loss, learning_rate, g_td=None, momentum=0.9):
 
   This function uses the scipy ode solver with the 'dopri5' algorithm.
 
-  [*] https://arxiv.org/abs/1806.07572
+  [*] https://arxiv.org/abs/1902.06720
 
   Example:
     ```python
@@ -469,16 +469,16 @@ def gp_inference(kernel_fn,
                  x_test,
                  get,
                  diag_reg=0.,
-                 compute_var=False):
+                 compute_cov=False):
   """Compute the mean and variance of the `posterior` of NNGP and NTK.
 
   Note that this method is equivalent to `gradient_descent_mse_gp` at infinite
   time. Example:
   ```python
   >>> predict = gradient_descent_mse_gp(kernel_fn, x_train, y_train, x_test,
-  >>>                                   get, diag_reg, compute_var)
+  >>>                                   get, diag_reg, compute_cov)
   >>> predict(np.inf) == predict(None) == gp_inference(kernel_fn, x_train,
-  >>>     y_train, x_test, get, diag_reg, compute_var)
+  >>>     y_train, x_test, get, diag_reg, compute_cov)
   ```
 
   Args:
@@ -490,7 +490,7 @@ def gp_inference(kernel_fn,
       tuple, or None. If `None` then both `nngp` and `ntk` predictions are
       returned.
     diag_reg: A float, representing the strength of the regularization.
-    compute_var: A boolean. If `True` computing both `mean` and `variance` and
+    compute_cov: A boolean. If `True` computing both `mean` and `variance` and
       only `mean` otherwise.
 
   Returns:
@@ -499,7 +499,7 @@ def gp_inference(kernel_fn,
   """
   if get is None:
     get = ('nngp', 'ntk')
-  kdd, ktd, ktt = _get_matrices(kernel_fn, x_train, x_test, get, compute_var)
+  kdd, ktd, ktt = _get_matrices(kernel_fn, x_train, x_test, get, compute_cov)
   gp_inference_mat = (_gp_inference_mat_jit_cpu if _is_on_cpu(kdd) else
                       _gp_inference_mat_jit)
   return gp_inference_mat(kdd, ktd, ktt, y_train, get, diag_reg)
@@ -535,16 +535,16 @@ def _gp_inference_mat(kdd,
     op = _inv_operator(kdd.nngp, diag_reg)
     pred_mean = _mean_prediction(op, ktd.nngp, y_train)
     if ktt is not None:
-      pred_var = _nngp_var(op, ktd.nngp, ktt)
-    out['nngp'] = (Gaussian(pred_mean, pred_var) if ktt is not None
-                   else pred_mean)
+      pred_cov = _nngp_cov(op, ktd.nngp, ktt)
+    out['nngp'] = (
+        Gaussian(pred_mean, pred_cov) if ktt is not None else pred_mean)
 
   if 'ntk' in get:
     op = _inv_operator(kdd.ntk, diag_reg)
     pred_mean = _mean_prediction(op, ktd.ntk, y_train)
     if ktt is not None:
-      pred_var = _ntk_var(op, ktd.ntk, kdd.nngp, ktd.nngp, ktt)
-    out['ntk'] = Gaussian(pred_mean, pred_var) if ktt is not None else pred_mean
+      pred_cov = _ntk_cov(op, ktd.ntk, kdd.nngp, ktd.nngp, ktt)
+    out['ntk'] = Gaussian(pred_mean, pred_cov) if ktt is not None else pred_mean
 
   return out
 
@@ -556,11 +556,11 @@ _gp_inference_mat_jit_cpu = jit(_gp_inference_mat, static_argnums=(4,),
                                 backend='cpu')
 
 
-def _get_matrices(kernel_fn, x_train, x_test, get, compute_var):
-  get = _get_dependency(get, compute_var)
+def _get_matrices(kernel_fn, x_train, x_test, get, compute_cov):
+  get = _get_dependency(get, compute_cov)
   kdd = kernel_fn(x_train, None, get)
   ktd = kernel_fn(x_test, x_train, get)
-  if compute_var:
+  if compute_cov:
     ktt = kernel_fn(x_test, x_test, 'nngp')
   else:
     ktt = None
@@ -574,7 +574,7 @@ def gradient_descent_mse_gp(kernel_fn,
                             x_test,
                             get,
                             diag_reg=0.0,
-                            compute_var=False):
+                            compute_cov=False):
   """Predicts the gaussian embedding induced by gradient descent with mse loss.
 
   This is equivalent to an infinite ensemble of networks after marginalizing
@@ -588,13 +588,13 @@ def gradient_descent_mse_gp(kernel_fn,
     get: string, the mode of the Gaussian process, either "nngp" or "ntk", or
       a tuple.
     diag_reg: A float, representing the strength of the regularization.
-    compute_var: A boolean. If `True` computing both `mean` and `variance` and
+    compute_cov: A boolean. If `True` computing both `mean` and `variance` and
       only `mean` otherwise.
 
   Returns:
     A function that predicts the gaussian parameters at t:
       predict(t) -> Gaussian(mean, variance).
-      If compute_var is False, only returns the mean.
+      If compute_cov is False, only returns the mean.
   """
   if get is None:
     get = ('nngp', 'ntk')
@@ -602,8 +602,13 @@ def gradient_descent_mse_gp(kernel_fn,
     # NOTE(schsam): This seems like an ugly solution that involves an extra
     # indirection. It might be nice to clean it up.
     return lambda t: gradient_descent_mse_gp(
-        kernel_fn, x_train, y_train, x_test,
-        diag_reg=diag_reg, get=(get,), compute_var=compute_var)(t)[0]
+        kernel_fn,
+        x_train,
+        y_train,
+        x_test,
+        diag_reg=diag_reg,
+        get=(get,),
+        compute_cov=compute_cov)(t)[0]
 
   _, get = canonicalize_get(get)
 
@@ -612,7 +617,7 @@ def gradient_descent_mse_gp(kernel_fn,
 
   eigenspace = {}
 
-  kdd, ktd, ktt = _get_matrices(kernel_fn, x_train, x_test, get, compute_var)
+  kdd, ktd, ktt = _get_matrices(kernel_fn, x_train, x_test, get, compute_cov)
   gp_inference_mat = (_gp_inference_mat_jit_cpu if _is_on_cpu(kdd) else
                       _gp_inference_mat_jit)
 
@@ -634,30 +639,41 @@ def gradient_descent_mse_gp(kernel_fn,
       evals, evecs = eigenspace['nngp']
       op_evals = -op_fn(evals, t)
       pred_mean = _mean_prediction_einsum(evecs, op_evals, ktd.nngp, y_train)
-      if compute_var:
+      if compute_cov:
         op_evals_x2 = -op_fn(evals, 2 * t)
-        pred_var = ktt - np.einsum(
+        pred_cov = ktt - np.einsum(
             'mj,ji,i,ki,lk->ml',
-            ktd.nngp, evecs, op_evals_x2, evecs, ktd.nngp, optimize=True)
+            ktd.nngp,
+            evecs,
+            op_evals_x2,
+            evecs,
+            ktd.nngp,
+            optimize=True)
 
-      out['nngp'] = Gaussian(pred_mean, pred_var) if compute_var else pred_mean
+      out['nngp'] = Gaussian(pred_mean, pred_cov) if compute_cov else pred_mean
 
     if 'ntk' in get:
       evals, evecs = eigenspace['ntk']
       op_evals = -op_fn(evals, t)
       pred_mean = _mean_prediction_einsum(evecs, op_evals, ktd.ntk, y_train)
-      if compute_var:
+      if compute_cov:
         # inline the covariance calculation with einsum.
-        pred_var = np.einsum(
+        term_1 = np.einsum(
+            'mi,i,ki,lk->ml', evecs, op_evals, evecs, ktd.ntk, optimize=True)
+        pred_cov = np.einsum(
+            'ji,jk,kl->il', term_1, kdd.nngp, term_1, optimize=True)
+        term_2 = np.einsum(
             'mj,ji,i,ki,lk->ml',
-            kdd.nngp, evecs, op_evals, evecs, ktd.ntk, optimize=True)
-        pred_var -= 2. * np.transpose(ktd.nngp)
-        pred_var = np.einsum(
-            'mj,ji,i,ki,kl->ml',
-            ktd.ntk, evecs, op_evals, evecs, pred_var, optimize=True)
-        pred_var = pred_var + ktt
+            ktd.ntk,
+            evecs,
+            op_evals,
+            evecs,
+            ktd.nngp,
+            optimize=True)
+        term_2 += np.transpose(term_2)
+        pred_cov += (-term_2 + ktt)
 
-      out['ntk'] = Gaussian(pred_mean, pred_var) if compute_var else pred_mean
+      out['ntk'] = Gaussian(pred_mean, pred_cov) if compute_cov else pred_mean
 
     returntype = named_tuple_factory('Gaussians', get)
     return returntype(*tuple(out[g] for g in get))
@@ -666,16 +682,15 @@ def gradient_descent_mse_gp(kernel_fn,
 
 
 ## Utility functions
-
-
-def _get_dependency(get, compute_var):
+def _get_dependency(get, compute_cov):
+  """Figure out dependency for get."""
   _, get = canonicalize_get(get)
   for g in get:
     if g not in ['nngp', 'ntk']:
       raise NotImplementedError(
           'Can only get either "nngp" or "ntk" predictions, got %s.' % g)
   get_dependency = ()
-  if 'nngp' in get or ('ntk' in get and compute_var):
+  if 'nngp' in get or ('ntk' in get and compute_cov):
     get_dependency += ('nngp',)
   if 'ntk' in get:
     get_dependency += ('ntk',)
@@ -787,29 +802,30 @@ def _mean_prediction_einsum(evecs, op_evals, g_td, y_train):
   return ufl(mean_pred)
 
 
-def _ntk_var(op, ntk_td, nngp_dd, nngp_td, nngp_tt):
+def _ntk_cov(op, ntk_td, nngp_dd, nngp_td, nngp_tt):
   """Compute the covariance in the ntk approximation."""
   # op(vec) here should compute \Theta^{-1} @ (I - e^{-\Theta dt}) @ vec
   # for the time dependent case and
   # op(vec) = \Theta^{-1} @ vec for the infinite time case.
-  # below implements Equation 15 from 1902.06720
-  var = op(np.transpose(ntk_td))
-  var = np.dot(nngp_dd, var)
-  var -= 2. * np.transpose(nngp_td)
-  var = op(var)
-  var = np.dot(ntk_td, var) + nngp_tt
-  return var
+  # below implements Equation 15 from https://arxiv.org/abs/1902.06720
+  term_1 = op(np.transpose(ntk_td))
+  cov = np.dot(nngp_dd, term_1)
+  cov = np.dot(np.transpose(term_1), cov)
+  term_2 = np.dot(ntk_td, op(np.transpose(nngp_td)))
+  term_2 += np.transpose(term_2)
+  cov += (-term_2 + nngp_tt)
+  return cov
 
 
-def _nngp_var(op, g_td, g_tt):
+def _nngp_cov(op, g_td, g_tt):
   """Compute the covariance in the nngp approximation."""
   # op(vec) here should compute K^{-1} @ (I - e^{-2 K dt}) @ vec
   # for the time dependent case or
   # op(vec) = K^{-1} @ vec
   # for infinite time.
-  # below implements Equation S23 from 1902.06720
-  var = op(np.transpose(g_td))
-  return g_tt - np.dot(g_td, var)
+  # below implements Equation S23 from https://arxiv.org/abs/1902.06720
+  cov = op(np.transpose(g_td))
+  return g_tt - np.dot(g_td, cov)
 
 
 def _make_expm1_fn(normalization):
@@ -857,7 +873,8 @@ def _jit_cpu(x):
 
 
 def max_learning_rate(kdd, num_outputs=-1, eps=1e-12):
-  """Computing the maximal feasible learning rate for infinite width NNs.
+  r"""Computing the maximal feasible learning rate for infinite width NNs.
+
   The network is assumed to be trained using SGD or full-batch GD with mean
   squared loss. The loss is assumed to have the form
   `1/(2 * batch_size * num_outputs) \|f(train_x) - train_y\|^2`. The maximal
@@ -871,6 +888,7 @@ def max_learning_rate(kdd, num_outputs=-1, eps=1e-12):
       analytic ntk, `num_outputs` must be provided. Otherwise `num_outputs=-1`
       and `num_outputs` is computed via the size of `kdd`.
     eps: A float to avoid zero divisor.
+
   Returns:
     The maximal feasible learning rate for infinite width NNs.
   """
@@ -893,4 +911,3 @@ def max_learning_rate(kdd, num_outputs=-1, eps=1e-12):
     max_eva = _eigh(kdd)[0][-1]
   lr = 2 * factor / (max_eva + eps)
   return lr
-

neural_tangents/tests/predict_test.py
@@ -22,6 +22,7 @@ from jax import test_util as jtu
 from jax.api import device_get
 from jax.api import grad
 from jax.api import jit
+from jax.api import vmap
 from jax.config import config
 from jax.experimental import optimizers
 from jax.lib import xla_bridge
@@ -489,7 +490,8 @@ class PredictTest(jtu.JaxTestCase):
                           for train, test, network in zip(
                               TRAIN_SHAPES[:-1], TEST_SHAPES[:-1], NETWORK[:-1])
                           for out_logits in OUTPUT_LOGITS))
-  def testNTKMeanPrediction(self, train_shape, test_shape, network, out_logits):
+  def testNTKMeanCovPrediction(self, train_shape, test_shape, network,
+                               out_logits):
 
     key = random.PRNGKey(0)
 
@@ -503,31 +505,30 @@ class PredictTest(jtu.JaxTestCase):
     key, split = random.split(key)
     x_test = np.cos(random.normal(split, test_shape))
     _, _, kernel_fn = _build_network(train_shape[1:], network, out_logits)
-    mean_pred, var = predict.gp_inference(
+    mean_pred, cov_pred = predict.gp_inference(
         kernel_fn,
         x_train,
         y_train,
         x_test,
         'ntk',
         diag_reg=0.,
-        compute_var=True)
+        compute_cov=True)
 
     if xla_bridge.get_backend().platform == 'tpu':
       eigh = np.onp.linalg.eigh
     else:
       eigh = np.linalg.eigh
 
-    self.assertEqual(var.shape[0], x_test.shape[0])
-    min_eigh = np.min(eigh(var)[0])
+    self.assertEqual(cov_pred.shape[0], x_test.shape[0])
+    min_eigh = np.min(eigh(cov_pred)[0])
     self.assertGreater(min_eigh + 1e-10, 0.)
 
     def mc_sampling(count=10):
-      empirical_mean = 0.
       key = random.PRNGKey(100)
       init_fn, f, _ = _build_network(train_shape[1:], network, out_logits)
       _kernel_fn = empirical.empirical_kernel_fn(f)
       kernel_fn = jit(lambda x1, x2, params: _kernel_fn(x1, x2, params, 'ntk'))
-
+      collect_test_predict = []
       for _ in range(count):
         key, split = random.split(key)
         _, params = init_fn(split, train_shape)
@@ -540,14 +541,21 @@ class PredictTest(jtu.JaxTestCase):
         fx_initial_test = f(params, x_test)
 
         _, fx_pred_test = predictor(1.0e8, fx_initial_train, fx_initial_test)
-        empirical_mean += fx_pred_test
-      return empirical_mean / count
+        collect_test_predict.append(fx_pred_test)
+      collect_test_predict = np.array(collect_test_predict)
+      mean_emp = np.mean(collect_test_predict, axis=0)
+      mean_subtracted = collect_test_predict - mean_emp
+      cov_emp = np.einsum(
+          'ijk,ilk->jl', mean_subtracted, mean_subtracted, optimize=True) / (
+              mean_subtracted.shape[0] * mean_subtracted.shape[-1])
+      return mean_emp, cov_emp
 
     atol = ATOL
     rtol = RTOL
-    mean_emp = mc_sampling(100)
+    mean_emp, cov_emp = mc_sampling(100)
 
     self.assertAllClose(mean_pred, mean_emp, True, rtol, atol)
+    self.assertAllClose(cov_pred, cov_emp, True, rtol, atol)
 
   @jtu.parameterized.named_parameters(
       jtu.cases_from_list({
@@ -588,7 +596,7 @@ class PredictTest(jtu.JaxTestCase):
         x_test,
         'ntk',
         diag_reg=0.,
-        compute_var=True)
+        compute_cov=True)
     assert isinstance(out, predict.Gaussian)
 
     out = predict.gp_inference(
@@ -598,7 +606,7 @@ class PredictTest(jtu.JaxTestCase):
         x_test,
         'nngp',
         diag_reg=0.,
-        compute_var=True)
+        compute_cov=True)
     assert isinstance(out, predict.Gaussian)
 
     out = predict.gp_inference(
@@ -607,7 +615,7 @@ class PredictTest(jtu.JaxTestCase):
         y_train,
         x_test, ('ntk',),
         diag_reg=0.,
-        compute_var=True)
+        compute_cov=True)
     assert len(out) == 1 and isinstance(out[0], predict.Gaussian)
 
     out = predict.gp_inference(
@@ -616,7 +624,7 @@ class PredictTest(jtu.JaxTestCase):
         y_train,
         x_test, ('ntk', 'nngp'),
         diag_reg=0.,
-        compute_var=True)
+        compute_cov=True)
     assert (len(out) == 2 and isinstance(out[0], predict.Gaussian) and
             isinstance(out[1], predict.Gaussian))
 
@@ -626,7 +634,7 @@ class PredictTest(jtu.JaxTestCase):
         y_train,
         x_test, ('nngp', 'ntk'),
         diag_reg=0.,
-        compute_var=True)
+        compute_cov=True)
     self.assertAllClose(out[0], out2[1], True)
     self.assertAllClose(out[1], out2[0], True)
 
@@ -673,7 +681,7 @@ class PredictTest(jtu.JaxTestCase):
         x_test,
         get,
         diag_reg=reg,
-        compute_var=True)
+        compute_cov=True)
 
     finite_prediction = prediction(np.inf)
     finite_prediction_none = prediction(None)
@@ -724,7 +732,7 @@ class PredictTest(jtu.JaxTestCase):
         x_test,
         diag_reg=reg,
         get=('NTK', 'NNGP'),
-        compute_var=True)
+        compute_cov=True)
 
     zero_prediction = prediction(0.0)
 
@@ -773,7 +781,7 @@ class PredictTest(jtu.JaxTestCase):
         x_test,
         diag_reg=reg,
         get='NTK',
-        compute_var=True)
+        compute_cov=True)
 
     ts = np.logspace(-2, 8, 10)
     ntk_predictions = [prediction(t).mean for t in ts]
@@ -793,7 +801,7 @@ class PredictTest(jtu.JaxTestCase):
         x_test,
         diag_reg=reg,
         get='NNGP',
-        compute_var=True)
+        compute_cov=True)
 
     ntk_nngp_predictions = [ntk_nngp_prediction(t).mean for t in ts]
 
@@ -810,9 +818,7 @@ class PredictTest(jtu.JaxTestCase):
         x_test,
         diag_reg=reg,
         get='NNGP',
-        compute_var=True)
-
-    nngp_predictions = [nngp_prediction(t).mean for t in ts]
+        compute_cov=True)
 
     # Create a hacked kernel function that always returns the nngp kernel
     def always_nngp(x1, x2, get=('nngp', 'ntk')):
@@ -829,7 +835,7 @@ class PredictTest(jtu.JaxTestCase):
         x_test,
         diag_reg=reg,
         get='NTK',
-        compute_var=True)
+        compute_cov=True)
 
     nngp_cov_predictions = [nngp_prediction(t).covariance for t in ts]
     nngp_ntk_cov_predictions = [nngp_ntk_prediction(t).covariance for t in ts]
@@ -837,6 +843,143 @@ class PredictTest(jtu.JaxTestCase):
     # Test if you use the ntk equations with the nngp, you get the same cov
     # Although, due to accumulation of numerical errors, only roughly.
     self.assertAllClose(nngp_cov_predictions, nngp_ntk_cov_predictions, True)
+
+  @jtu.parameterized.named_parameters(
+      jtu.cases_from_list({
+          'testcase_name':
+              '_train={}_test={}_network={}_logits={}'.format(
+                  train, test, network, out_logits),
+          'train_shape':
+              train,
+          'test_shape':
+              test,
+          'network':
+              network,
+          'out_logits':
+              out_logits,
+      }
+                          for train, test, network in zip(
+                              TRAIN_SHAPES[:-1], TEST_SHAPES[:-1], NETWORK[:-1])
+                          for out_logits in OUTPUT_LOGITS))
+  def testNTKPredCovPosDef(self, train_shape, test_shape, network, out_logits):
+    key = random.PRNGKey(0)
+
+    key, split = random.split(key)
+    x_train = np.cos(random.normal(split, train_shape))
+
+    key, split = random.split(key)
+    y_train = np.array(
+        random.bernoulli(split, shape=(train_shape[0], out_logits)), np.float32)
+
+    key, split = random.split(key)
+    x_test = np.cos(random.normal(split, test_shape))
+    _, _, ker_fun = _build_network(train_shape[1:], network, out_logits)
+
+    reg = 1e-7
+    ntk_predictions = predict.gradient_descent_mse_gp(
+        ker_fun,
+        x_train,
+        y_train,
+        x_test,
+        diag_reg=reg,
+        get='ntk',
+        compute_cov=True)
+
+    ts = np.logspace(-2, 8, 10)
+
+    ntk_cov_predictions = [ntk_predictions(t).covariance for t in ts]
+
+    if xla_bridge.get_backend().platform == 'tpu':
+      eigh = np.onp.linalg.eigh
+    else:
+      eigh = np.linalg.eigh
+
+    check_symmetric = np.array(
+        [np.max(np.abs(cov - cov.T)) for cov in ntk_cov_predictions])
+    check_pos_evals = np.min(
+        np.array([eigh(cov)[0] + 1e-10 for cov in ntk_cov_predictions]))
+
+    self.assertAllClose(check_symmetric, np.zeros_like(check_symmetric), True)
+    self.assertGreater(check_pos_evals, 0., True)
+
+  @jtu.parameterized.named_parameters(
+      jtu.cases_from_list({
+          'testcase_name':
+              '_train={}_test={}_network={}_logits={}'.format(
+                  train, test, network, out_logits),
+          'train_shape':
+              train,
+          'test_shape':
+              test,
+          'network':
+              network,
+          'out_logits':
+              out_logits,
+      }
+                          for train, test, network in zip(
+                              TRAIN_SHAPES[:-1], TEST_SHAPES[:-1], NETWORK[:-1])
+                          for out_logits in OUTPUT_LOGITS))
+  def testTrainedEnsemblePredCov(self, train_shape, test_shape, network,
+                                 out_logits):
+    if xla_bridge.get_backend().platform == 'gpu' and config.read(
+        'jax_enable_x64'):
+      raise jtu.SkipTest('Not running GPU x64 to save time.')
+    training_steps = 5000
+    learning_rate = 1.0
+    ensemble_size = 50
+
+    init_fn, apply_fn, ker_fn = stax.serial(
+        stax.Dense(1024, W_std=1.2, b_std=0.05), stax.Erf(),
+        stax.Dense(out_logits, W_std=1.2, b_std=0.05))
+
+    opt_init, opt_update, get_params = optimizers.sgd(learning_rate)
+    opt_update = jit(opt_update)
+
+    key = random.PRNGKey(0)
+    key, = random.split(key, 1)
+
+    key, split = random.split(key)
+    x_train = np.cos(random.normal(split, train_shape))
+
+    key, split = random.split(key)
+    y_train = np.array(
+        random.bernoulli(split, shape=(train_shape[0], out_logits)), np.float32)
+    train = (x_train, y_train)
+    key, split = random.split(key)
+    x_test = np.cos(random.normal(split, test_shape))
+
+    ensemble_key = random.split(key, ensemble_size)
+
+    loss = jit(lambda params, x, y: 0.5 * np.mean((apply_fn(params, x) - y)**2))
+    grad_loss = jit(lambda state, x, y: grad(loss)(get_params(state), x, y))
+
+    def train_network(key):
+      _, params = init_fn(key, (-1,) + train_shape[1:])
+      opt_state = opt_init(params)
+      for i in range(training_steps):
+        opt_state = opt_update(i, grad_loss(opt_state, *train), opt_state)
+
+      return get_params(opt_state)
+
+    params = vmap(train_network)(ensemble_key)
+
+    ensemble_fx = vmap(apply_fn, (0, None))(params, x_test)
+    ensemble_loss = vmap(loss, (0, None, None))(params, x_train, y_train)
+    ensemble_loss = np.mean(ensemble_loss)
+    self.assertLess(ensemble_loss, 1e-5, True)
+
+    mean_emp = np.mean(ensemble_fx, axis=0)
+    mean_subtracted = ensemble_fx - mean_emp
+    cov_emp = np.einsum(
+        'ijk,ilk->jl', mean_subtracted, mean_subtracted, optimize=True) / (
+            mean_subtracted.shape[0] * mean_subtracted.shape[-1])
+
+    reg = 1e-7
+    ntk_predictions = predict.gp_inference(
+        ker_fn, x_train, y_train, x_test, 'ntk', reg, compute_cov=True)
+
+    self.assertAllClose(mean_emp, ntk_predictions.mean, True, RTOL, ATOL)
+    self.assertAllClose(cov_emp, ntk_predictions.covariance, True, RTOL, ATOL)
 
   def testPredictOnCPU(self):
     x_train = random.normal(random.PRNGKey(1), (10, 4, 5, 3))