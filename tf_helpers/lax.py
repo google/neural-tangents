1ebaf7d9e76b295479a16113ebcf86c9af2e8173
DarrenZhang01 <18633059886@163.com>
2020-08-26 20:09:52 -0400

    Now since Trax extensions is already there, call tf_dot_general directly inside lax dot_general

--- tf_helpers/lax.py
+++ tf_helpers/lax.py
@@ -21,6 +21,7 @@ import numpy as onp
 from tensorflow.python.ops import numpy_ops as np
 import tensorflow as tf
 from tensorflow import nn
+from tf_helpers.extensions import tf_dot_general
 import sys
 
 _max = builtins.max
@@ -262,40 +263,7 @@ def dot_general(lhs, rhs, dimension_numbers, precision=None):
   Returns:
     An array that contains the result.
   """
-  char_list = list(string.ascii_lowercase)
-  char_list = char_list[8:] + char_list[:8]
-  lhs_rank, rhs_rank = len(lhs.shape), len(rhs.shape)
-  lhs_rep = char_list[:lhs_rank]
-  rhs_rep = char_list[lhs_rank:lhs_rank+rhs_rank]
-  contraction, batch = dimension_numbers
-  lhs_contraction, rhs_contraction = contraction
-  if len(lhs_contraction) != len(rhs_contraction):
-    raise ValueError("The input matrices are required to have the same number "
-                     "of contraction dimensions, but got: lhs {}, rhs: {}".format(
-                     len(lhs_contraction), len(rhs_contraction)))
-  lhs_batch, rhs_batch = batch
-  if len(lhs_batch) != len(rhs_batch):
-    raise ValueError("The input matrices are required to have the same number "
-                     "of batch dimensions, but got: lhs {}, rhs: {}".format(
-                     len(lhs_batch), len(rhs_batch)))
-
-  if len(lhs_batch) == 0 and len(rhs_batch) == 0:
-    return np.asarray(
-      tf.tensordot(lhs, rhs, axes=(list(lhs_contraction), list(rhs_contraction))))
-
-  if (lhs_rank == rhs_rank == 3 and lhs_batch == (0,) and rhs_batch == (0,)
-      and lhs_contraction == (2,) and rhs_contraction == (1,)):
-    return np.asarray(tf.linalg.matmul(lhs, rhs))
-
-  for i in range(len(lhs_contraction)):
-    rhs_rep[rhs_contraction[i]] = lhs_rep[lhs_contraction[i]]
-  for i in range(len(lhs_batch)):
-    rhs_rep[rhs_batch[i]] = lhs_rep[lhs_batch[i]]
-
-  output_rep = _compose_output_rep(lhs_rep, rhs_rep, lhs_contraction,
-                                  rhs_contraction, lhs_batch, rhs_batch)
-  equation = ''.join(lhs_rep) + ',' + ''.join(rhs_rep) + "->" + output_rep
-  return np.asarray(tf.einsum(equation, lhs, rhs))
+  return tf_dot_general(lhs, rhs, dimension_numbers, precision)
 
 
 def reduce_window(inputs, init_value, reducer, window_dimensions, strides,
@@ -396,39 +364,6 @@ def _conv_transpose_padding(k, s, padding):
     raise ValueError('Padding mode must be `SAME` or `VALID`.')
   pad_b = pad_len - pad_a
   return pad_a, pad_b
-
-
-def _minus(a, b):
-  return [x for x in a if x not in b]
-
-
-def _compose_output_rep(lhs_rep, rhs_rep, lhs_contraction, rhs_contraction,
-                        lhs_batch, rhs_batch):
-  """ Compose the output string representation.
-
-  e.g., ij, jk, (((1,), (0,)), ((), ())) -> ik
-        aij, ajk, (((2,), (1,)), ((0,), (0,))) -> aik
-
-  Args:
-    lhs_rep: A string representation for the left-hand side input array
-    rhs_rep: A string representation for the right-hand side input array
-    lhs_contraction: Sequence[int] (the contraction dimensions of lhs)
-    rhs_contraction: Sequence[int] (the contraction dimensions of rhs)
-    lhs_batch: Sequence[int] (the batch dimensions of lhs)
-    rhs_batch: Sequence[int] (the batch dimensions of rhs)
-
-  Returns:
-    A string representation of the result array.
-  """
-  output_rep = []
-  for dim in lhs_batch:
-    output_rep.append(lhs_rep[dim])
-
-  for i in _minus(range(len(lhs_rep)), lhs_batch + lhs_contraction):
-    output_rep.append(lhs_rep[i])
-  for i in _minus(range(len(rhs_rep)), rhs_batch + rhs_contraction):
-    output_rep.append(rhs_rep[i])
-  return ''.join(output_rep)
 
 
 def _conv_general_param_type_converter(window_strides, lhs_dilation, rhs_dilation):
