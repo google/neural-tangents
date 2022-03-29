61bc32de36cc73bbca46207d37b823e79e17316f
insuhan <hawki17@kaist.ac.kr>
2022-03-30 06:14:18 +0900

    Add JAXopt package

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 INSTALL_REQUIRES = [
     'jax>=0.3',
     'frozendict>=2.3',
-    'typing_extensions>=4.0.1'
+    'typing_extensions>=4.0.1',
+    'jaxopt>=0.3.1',
 ]
 
 
