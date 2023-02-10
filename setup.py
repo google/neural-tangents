0272225ee56d8878f5bde30f84d58e058d06b348
Roman Novak <romann@google.com>
2023-02-10 12:26:01 -0800

    Version bump to make NT work on M1 macs (https://github.com/google/neural-tangents/issues/171). BREAKING: drop support for Python 3.7 (necessary due to JAX and TF2JAX policy).
    Also fix some linter errors.
    
    PiperOrigin-RevId: 508726120

--- setup.py
+++ setup.py
@@ -26,10 +26,10 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 
 INSTALL_REQUIRES = [
-    'jax>=0.3.13',
+    'jax>=0.4.3',
     'frozendict>=2.3',
     'typing_extensions>=4.0.1',
-    'tf2jax>=0.3.0',
+    'tf2jax>=0.3.3',
 ]
 
 
@@ -44,20 +44,20 @@ def _get_version() -> str:
   """Returns the package version.
 
   Adapted from:
-  https://github.com/deepmind/dm-haiku/blob/d4807e77b0b03c41467e24a247bed9d1897d336c/setup.py#L22
+  https://github.com/deepmind/tf2jax/blob/41ea640c7525ed42f7dcd02937b1f308f8949521/setup.py#L24
 
   Returns:
     Version number.
   """
-  path = 'neural_tangents/__init__.py'
-  version = '__version__'
-  with open(path) as fp:
+  current_dir = os.path.dirname(os.path.abspath(__file__))
+
+  with open(os.path.join(current_dir, 'neural_tangents', '__init__.py')) as fp:
     for line in fp:
-      if line.startswith(version):
-        g = {}
-        exec(line, g)
-        return g[version]
-    raise ValueError(f'`{version}` not defined in `{path}`.')
+      if line.startswith('__version__') and '=' in line:
+        version = line[line.find('=') + 1:].strip(' \'"\n')
+        if version:
+          return version
+    raise ValueError('`__version__` not found in `neural_tangents/__init__.py`')
 
 
 setuptools.setup(
@@ -73,27 +73,51 @@ setuptools.setup(
     url='https://github.com/google/neural-tangents',
     download_url='https://pypi.org/project/neural-tangents/',
     project_urls={
-        'Source Code': 'https://github.com/google/neural-tangents',
-        'Paper': 'https://arxiv.org/abs/1912.02803',
-        'Finite Width NTK paper': 'https://arxiv.org/abs/2206.08720',
-        'Video': 'https://iclr.cc/virtual_2020/poster_SklD9yrFPS.html',
-        'Finite Width NTK video': 'https://youtu.be/8MWOhYg89fY?t=10984',
-        'Documentation': 'https://neural-tangents.readthedocs.io/en/latest/?badge=latest',
-        'Bug Tracker': 'https://github.com/google/neural-tangents/issues',
-        'Release Notes': 'https://github.com/google/neural-tangents/releases',
-        'PyPi': 'https://pypi.org/project/neural-tangents/',
-        'Linux Tests': 'https://github.com/google/neural-tangents/actions/workflows/linux.yml',
-        'macOS Tests': 'https://github.com/google/neural-tangents/actions/workflows/macos.yml',
-        'Pytype': 'https://github.com/google/neural-tangents/actions/workflows/pytype.yml',
-        'Coverage': 'https://app.codecov.io/gh/google/neural-tangents'
+        'Source Code':
+            'https://github.com/google/neural-tangents',
+
+        'Paper':
+            'https://arxiv.org/abs/1912.02803',
+
+        'Finite Width NTK paper':
+            'https://arxiv.org/abs/2206.08720',
+
+        'Video':
+            'https://iclr.cc/virtual_2020/poster_SklD9yrFPS.html',
+
+        'Finite Width NTK video':
+            'https://youtu.be/8MWOhYg89fY?t=10984',
+
+        'Documentation':
+            'https://neural-tangents.readthedocs.io/en/latest/?badge=latest',
+
+        'Bug Tracker':
+            'https://github.com/google/neural-tangents/issues',
+
+        'Release Notes':
+            'https://github.com/google/neural-tangents/releases',
+
+        'PyPi':
+            'https://pypi.org/project/neural-tangents/',
+
+        'Linux Tests':
+            'https://github.com/google/neural-tangents/actions/workflows/linux.yml',
+
+        'macOS Tests':
+            'https://github.com/google/neural-tangents/actions/workflows/macos.yml',
+
+        'Pytype':
+            'https://github.com/google/neural-tangents/actions/workflows/pytype.yml',
+
+        'Coverage':
+            'https://app.codecov.io/gh/google/neural-tangents'
     },
     packages=setuptools.find_packages(exclude=('presentation',)),
     long_description=long_description,
     long_description_content_type='text/markdown',
     description='Fast and Easy Infinite Neural Networks in Python',
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     classifiers=[
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
